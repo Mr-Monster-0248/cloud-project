020842efb51dd931d9184889c46387c722203c59
Thibault LEPEZ <thibault.lepez@efrei.net>
2022-03-04 12:47:19 +0100

    feat: add is owner middleware

--- src/services/auth/check-is-authenticated.service.ts
+++ src/services/auth/check-is-authenticated.service.ts
@@ -1,14 +1,12 @@
-import { onRequestHookHandler } from 'fastify';
+import { preHandlerHookHandler } from 'fastify';
 import { getOneUserByToken } from '../database/user-queries.service';
 
-export const checkIsAuthenticated: onRequestHookHandler = (req, res, done) => {
+export const checkIsAuthenticated: preHandlerHookHandler = (req, res, done) => {
   const session = req.session;
-  console.log(req.headers)
   if (session && session.token && session.userId) {
     done();
   } else if (req.headers['authorization']) {
     const token = (req.headers['authorization'] as string).split(' ')[1];
-    console.log(token)
     getOneUserByToken(token).then((user) => {
       req.session.userId = user.userId;
       req.session.token = user.token;
---
+++ src/services/auth/check-is-owner.service.ts
@@ -0,0 +1,24 @@
+import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify';
+import { getOneRestaurantById } from '../database/restaurant-queries.service';
+
+export const checkIsOwner = (
+  req: FastifyRequest<{ Params: { restaurantId: number } }>,
+  res: FastifyReply,
+  done: HookHandlerDoneFunction
+) => {
+  getOneRestaurantById(req.params.restaurantId)
+    .then((restaurant) => {
+      if (restaurant.owner.userId === req.session.userId) {
+        done();
+      } else {
+        res.log.warn(
+          `user ${req.session.userId} tried to access restaurant ${req.params.restaurantId}`
+        );
+        res.code(403).send(new Error('You need to be owner of the restaurant'));
+      }
+    })
+    .catch((err) => {
+      res.log.error(err);
+      res.code(404).send(new Error('Restaurant not fount'));
+    });
+};
