a850825326770990402a882afde24a171858cffa
Ruben Nabet <rubenabet@gmail.com>
2022-03-13 21:33:54 +0100

    tests: tweak coverage collection settings

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,10 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   coverageReporters: ["text", "html"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/types/**/*.ts",        // Ignored - No use checking type declarations for coverage
+    "!src/utils/checkLogPath.ts" // Ignored - Would require changing the value of NODE_ENV
+  ],
   testPathIgnorePatterns: [".js"],
 };
